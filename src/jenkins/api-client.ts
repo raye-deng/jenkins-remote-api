6c0cf9c8116c425ea3f5b13e3e5ccb4534c134ad
raye.deng <raye.deng@ringcentral.com>
2025-04-27 15:27:54 +0800

    remove useless logs

--- src/jenkins/api-client.ts
+++ src/jenkins/api-client.ts
@@ -39,7 +39,7 @@ export default class APIClient {
                 if (location) {
                     response.config.baseURL = undefined;
                     response.config.url = location;
-                    this.logger.info(`re-location to :${location}`);
+                    //this.logger.info(`re-location to :${location}`);
                     return await this.client.request(response.config);
                 }
             }
@@ -50,7 +50,7 @@ export default class APIClient {
                 await this.getCrumb();
                 config.baseURL = undefined;
                 config["isRetry"] = true;
-                this.logger.warn(`refresh crumb and retrying api :${config.url}`);
+               // this.logger.warn(`refresh crumb and retrying api :${config.url}`);
                 if (config["isRetry"]) {
                     error.message = `${error.message}, request already retry after first time failure.`;
                     return Promise.reject(error);
@@ -72,7 +72,7 @@ export default class APIClient {
                 rawErrorMessage = rawErrorMessage ? rawErrorMessage[0].replace(/(<([^>]+)>)/ig, " ").replace(/[\r\n]/g, " ") : rawErrorMessage;
             }
 
-            this.logger.error(`api invoke failed,[${config.method}] ${config.url},${data && typeof data == "string" ? data : JSON.stringify(data)}`);
+            // this.logger.error(`api invoke failed,[${config.method}] ${config.url},${data && typeof data == "string" ? data : JSON.stringify(data)}`);
             const stack = new Error().stack;
             error = Object.assign(error, { message: `${error.message} ${rawErrorMessage ? "(html error message:" + rawErrorMessage + ")" : ''}`, stack });
             return Promise.reject(error);
